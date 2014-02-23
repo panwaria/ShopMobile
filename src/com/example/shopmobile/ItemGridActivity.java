f644b9be1dc12fabcd8c4130d6f7b4b11542f370
Shishir Kumar Prasad <skprasad@cs.wisc.edu>
2014-02-22 20:39:14 -0800

    Added support for downloading image bitmap from url

--- src/com/example/shopmobile/ItemGridActivity.java
+++ src/com/example/shopmobile/ItemGridActivity.java
@@ -1,13 +1,21 @@
 package com.example.shopmobile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import com.example.shopmobile.model.Item;
 import com.example.shopmobile.service.CatalogSearchService;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 /**
  * Renders the appropriate items in a grid format.
@@ -31,5 +39,23 @@ public class ItemGridActivity extends Activity {
 	    
 	    List<Item> catalogItems = CatalogSearchService.searchItemsInCatalog(category, keyword);
 	    Log.i("ItemGridActivity", "Found " + catalogItems.size() + " catalog items.");
+	    
+	    for(Item item : catalogItems)
+	    {
+	    	String url = item.getAttrValuesMap().get(Constants.ITEM_IMG_URL);
+	    	loadImageForURL(url);
+	    }
+	}
+	
+	private void loadImageForURL(String url) {
+		try {
+			ImageView imgView = (ImageView) findViewById(R.id.image);
+			Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+			imgView.setImageBitmap(bitmap);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
