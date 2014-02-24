46f25f4365995bbe6c7601a0c2949790a3b1d5ea
Prakhar Panwaria <prakhar.panwaria9@gmail.com>
2014-02-24 01:04:28 -0600

    Reformatted files.

--- src/com/example/shopmobile/service/CatalogSearchService.java
+++ src/com/example/shopmobile/service/CatalogSearchService.java
@@ -21,36 +21,44 @@ import com.google.common.collect.Maps;
  * Search API for searching the catalog for items based on relevant keywords.
  * 
  * @author excelsior
- *
+ * 
  */
-public class CatalogSearchService {
+public class CatalogSearchService
+{
 
 	/**
 	 * Returns a list of items corresponding to a category and a search keyword
-	 * @param category	- Category of the item. e.g. Books
-	 * @param keyword	- Keyword to search.	e.g. J.K.Rowling. Searched for all the books which
-	 * 					have keyword "J.K.Rowling" somewhere in the attributes.
+	 * 
+	 * @param category
+	 *            - Category of the item. e.g. Books
+	 * @param keyword
+	 *            - Keyword to search. e.g. J.K.Rowling. Searched for all the
+	 *            books which have keyword "J.K.Rowling" somewhere in the
+	 *            attributes.
 	 */
 	public static List<Item> searchItemsInCatalog(String category, String keyword)
 	{
 		// TODO : Fetch JSON data using an apt url
 		List<Item> items = Lists.newArrayList();
-		
+
 		// TODO : Hardcoded data as of now. Fix this !!
 		Map<String, String> item1AttrMap = Maps.newHashMap();
-		item1AttrMap.put(Constants.ITEM_URL, "http://highered.mcgraw-hill.com/sites/dl/free/0070131511/cover/cormen-lg_cover.jpg");
+		item1AttrMap
+				.put(Constants.ITEM_URL,
+						"http://highered.mcgraw-hill.com/sites/dl/free/0070131511/cover/cormen-lg_cover.jpg");
 		items.add(new Item(1, item1AttrMap));
-		
+
 		Map<String, String> item2AttrMap = Maps.newHashMap();
 		item2AttrMap.put(Constants.ITEM_URL, "http://pages.cs.wisc.edu/~dbbook/images/book3ed.jpg");
 		items.add(new Item(2, item2AttrMap));
-		
+
 		return items;
 	}
-	
+
 	// TODO
 	/**
 	 * Returns the fetch url based on the category and keyword parameters.
+	 * 
 	 * @param category
 	 * @param keyword
 	 * @return
@@ -59,32 +67,41 @@ public class CatalogSearchService {
 	{
 		return null;
 	}
-	
+
 	/**
-	 * http://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java
+	 * http://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from
+	 * -a-url-in-java
+	 * 
 	 * @param url
 	 * @return
-	 * @throws IOException 
-	 * @throws MalformedURLException 
-	 * @throws JSONException 
+	 * @throws IOException
+	 * @throws MalformedURLException
+	 * @throws JSONException
 	 */
-	private static JSONObject getJSONFromURL(String url) throws MalformedURLException, IOException, JSONException
+	private static JSONObject getJSONFromURL(String url) throws MalformedURLException, IOException,
+			JSONException
 	{
-	    InputStream is = new URL(url).openStream();
-	    try {
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	      String jsonText = readAll(rd);
-	      JSONObject json = new JSONObject(jsonText);
-	      return json;
-	    } finally {
-	      is.close();
-	    }		
+		InputStream is = new URL(url).openStream();
+		try
+		{
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is,
+					Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			JSONObject json = new JSONObject(jsonText);
+			return json;
+		} 
+		finally
+		{
+			is.close();
+		}
 	}
-	
-	private static String readAll(Reader rd) throws IOException {
+
+	private static String readAll(Reader rd) throws IOException
+	{
 		StringBuilder sb = new StringBuilder();
 		int cp;
-		while ((cp = rd.read()) != -1) {
+		while ((cp = rd.read()) != -1)
+		{
 			sb.append((char) cp);
 		}
 		return sb.toString();
--- src/com/example/shopmobile/service/StripePaymentService.java
+++ src/com/example/shopmobile/service/StripePaymentService.java
@@ -1,9 +1,7 @@
 package com.example.shopmobile.service;
 
 import java.util.Map;
-
 import android.util.Log;
-
 import com.example.shopmobile.Constants;
 import com.google.common.collect.Maps;
 import com.stripe.Stripe;
@@ -18,61 +16,70 @@ import com.stripe.model.Charge;
  * Provides functionality to accept payements using Stripe API.
  * 
  * @author excelsior
- *
+ * 
  */
-public class StripePaymentService {
+public class StripePaymentService
+{
 	/**
 	 * Debit the amount from the credit card using Stripe API.
 	 * 
 	 * @return
 	 */
-	public static final boolean debit(double amount, String currency, long cardNumber, int expMonth, int expYear, String cvc)
+	public static final boolean debit(double amount, String currency, long cardNumber,
+			int expMonth, int expYear, String cvc)
 	{
 		Stripe.apiKey = Constants.STRIPE_KEY;
-		
+
 		Map<String, Object> cardParams = Maps.newHashMap();
 		cardParams.put(Constants.CARD_NUMBER, cardNumber);
 		cardParams.put(Constants.CARD_EXP_MONTH, expMonth);
 		cardParams.put(Constants.CARD_EXP_YEAR, expYear);
 		cardParams.put(Constants.CARD_CVC, cvc);
-		
+
 		Map<String, Object> chargeParams = Maps.newHashMap();
 		chargeParams.put(Constants.CHARGE_AMOUNT, amount);
 		chargeParams.put(Constants.CHARGE_CURRENCY, currency);
 		chargeParams.put(Constants.CHARGE_CARD, cardParams);
-		
+
 		boolean isDebitSuccess = true;
 		Exception err = null;
 		Charge charge = null;
-		try {
+		try
+		{
 			charge = Charge.create(chargeParams);
-		} catch (AuthenticationException e) {
+		} catch (AuthenticationException e)
+		{
 			isDebitSuccess = false;
 			err = e;
-		} catch (InvalidRequestException e) {
+		} catch (InvalidRequestException e)
+		{
 			isDebitSuccess = false;
 			err = e;
-		} catch (APIConnectionException e) {
+		} catch (APIConnectionException e)
+		{
 			isDebitSuccess = false;
 			err = e;
-		} catch (CardException e) {
+		} catch (CardException e)
+		{
 			isDebitSuccess = false;
 			err = e;
-		} catch (APIException e) {
+		} catch (APIException e)
+		{
 			isDebitSuccess = false;
 			err = e;
 		}
-		
-		if(isDebitSuccess) {
-			Log.i("StripePaymentService", "Credit card debited successfully for " + amount + " " + currency + 
-				  "Charge : " + charge.toString());
+
+		if (isDebitSuccess)
+		{
+			Log.i("StripePaymentService", "Credit card debited successfully for " + amount + " "
+					+ currency + "Charge : " + charge.toString());
+		} else
+		{
+			Log.e("StripePaymentService", "Failed to debit credit card for " + amount + " "
+					+ currency + ". Reason : " + err.getStackTrace());
 		}
-		else {
-			Log.e("StripePaymentService", "Failed to debit credit card for " + amount + " " + currency + 
-				  ". Reason : " + err.getStackTrace());
-		}
-		
+
 		return isDebitSuccess;
 	}
-	
+
 }
